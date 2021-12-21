385f5d1ee6589e84678e7e8a19daf27514d096ca
kewalb <bkewal@gmail.com>
2021-12-21 19:57:06 +0530

    Added few comments

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ const getDetails = () => {
   xhr.onload = () => {
     const details = xhr.response;
     for (let detail of details) {
-      console.log(detail.name, detail.region, detail.subregion, detail.population);
+      console.log(
+        detail.name,
+        detail.region,
+        detail.subregion,
+        detail.population
+      );
     }
   };
   xhr.send();
@@ -15,23 +20,24 @@ const getDetails = () => {
 // invoking the function
 getDetails();
 
-
-
 // displaying the country flags in console from rest countries api
 
-const getFlag = ()=> {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://restcountries.com/v3.1/all");
-    xhr.responseType = "json";
-    xhr.onload = () => {
-        const flags = xhr.response;
-        for (let flag of flags){
-            console.log(flag.flags);
-        }
-
-    };
-    xhr.send();
+const getFlag = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://restcountries.com/v3.1/all");
+  xhr.responseType = "json";
+  xhr.onload = () => {
+    const flags = xhr.response;
+    for (let flag of flags) {
+      console.log(flag.flags);
+    }
+  };
+  xhr.send();
 };
 
 // invoking the function
 getFlag();
+
+// How to compare two JSON have the same properties without order? var obj1 = { name: "Person 1", age:5 }; var obj2 = { age:5, name: "Person 1" };
+
+// _.isEqual(obj1, obj2)
